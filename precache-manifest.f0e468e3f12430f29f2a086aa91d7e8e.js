8e5d546c5eee5d30008b54a112ac8b00c394f79d
karo0wk <karo0wk@gmail.com>
2019-10-02 16:21:36 -0500

    Updates

---
+++ precache-manifest.f0e468e3f12430f29f2a086aa91d7e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15df2421497a6c994d5cd83bbb1d19a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d044c9375513deb0d2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "310b64553ffca4f3827f",
+    "url": "/static/js/2.c8972d9e.chunk.js"
+  },
+  {
+    "revision": "17d044c9375513deb0d2",
+    "url": "/static/js/main.a946e052.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
