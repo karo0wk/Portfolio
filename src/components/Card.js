39588812967e8f1bf7dc5c6af527599b6bbe43e7
karo0wk <karo0wk@gmail.com>
2019-11-29 10:25:50 -0600

    Cambios finales

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,11 @@ class Card extends React.Component {
 								<div className="Thumb">
 								<a href={this.props.url}><img  className="imageTipe" src={this.props.image} alt="Logo" /></a>
 								</div>
+								<div className="ContainerDescription">
 								<h3 className="titleCard">{this.props.title}</h3>
 								<p className="paragraph">{this.props.description}</p>
+	                            <p2 className="paragraph">{this.props.tools}</p2>
+								</div>
 							</article>
     );
     };
--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -11,13 +11,16 @@ function SectionTwo() {
 <div>
 						<h2>Recent Work</h2>
 						    <div className="row">
-							<Card title="Lab-Notes" description="A web-app to take notes." image={Notes} url="https://github.com/CarolinaPerezFlores/GDL003-lab-notes"/>
+							<Card title="Take Notes" description="This is a web-app that allows you to make notes, save them in a database and edit them." 
+							tools="For this project were used: React.js, Firebase, and Bootstrap." image={Notes} url="https://github.com/CarolinaPerezFlores/GDL003-lab-notes"/>
 							</div>
 							<div className="row">
-							<Card title="Parachute Game" description="A web-app to play in your cellphone." image={ParachuteGame} url="https://github.com/CarolinaPerezFlores/GDL003-cipher"/>
+							<Card title="Parachute Game" description="This application designed for cell phones gives you the ability to play to save the paratrooper before it falls." 
+							tools="For this project were used: React Native" image={ParachuteGame} url="https://github.com/CarolinaPerezFlores/GDL003-cipher"/>
 							</div>
 							<div className="row">
-							<Card title="Burger Queen" description="A Web-App, for ordering restaurants." image={Burger} url="https://github.com/CarolinaPerezFlores/GDL003-Burger-Queen-Back-End"/>
+							<Card title="Burger Queen" description="A Web-App, to take orders in restaurants, all electronically. This application can facilitate the process of an order, mainly shows the menus and products offered to the customer, can create, add, delete orders, and finally make a total sum of the cost of the products provided." 
+							tools="For this project were used: Mongo Database, Express, Post Man and React.js. "image={Burger} url="https://github.com/CarolinaPerezFlores/GDL003-Burger-Queen-Back-End"/>
 							</div>
 						
 						<ul className="actions">
